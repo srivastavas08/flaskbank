053521dc65e3164424f564eb41fa4e1b78e1529f
srivastavas08 <59473943+srivastavas08@users.noreply.github.com>
2020-07-03 13:32:48 +0530

    Datebase Connected

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-import os 
+import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or b'jdncknzkcncomcozjvoksijvchcckcmm'
-
-
-    MONGODB_SETTINGS = {'db' : 'UTA_Enrollment'}
-
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY") or b'jdncknzkcncomcozjvoksijvchcckcmm'
+    MONGODB_SETTINGS = {
+    'db': 'FlaskBank',
+    'host': 'mongodb+srv://cluster-flaskbank.f042j.gcp.mongodb.net/FlaskBank?retryWrites=true&w=majority',
+    'username':'mongoengine',
+    'password':'SHIvam7426'
+    }
